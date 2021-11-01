be186a8746cc9057df8338be82950d10e5ef4ba5
Basabe-Gabriel <92063962+Basabe-Gabriel@users.noreply.github.com>
2021-11-01 19:56:11 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+let mes: string;
+//let mes = prompt("ingrese el mes: ");
+switch (mes) {
+  case "(1 || 3 || 4 || 7 || 8 || 10 ||12) ": {
+    console.log("el mes se compone de 30 dias");
+    break;
+  }
+  case "(4== || 6== || 9==) ": {
+    console.log("el mes se compone de 31 dias");
+    break;
+  }
+  case "2": {
+    console.log("el mes se compone de 28 o 29 dias");
+    break;
+  }
+}
